53b294cf86efe3f2f5312edab366616335c8a902
Перепелицын Максим Валерьевич <18446214@cab-wsm-0005523.sigma.sbrf.ru>
2020-04-01 12:58:23 +0300

    starting the work on GameCycle

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,10 @@
+import TDGame.*;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-
-
-
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int n = 0, m = 0;
-
-        n = scan.nextInt();
-        m = scan.nextInt();
+        GameCycle.gameCycle();
     }
 }
